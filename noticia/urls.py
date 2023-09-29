200f06b487642b831296850382dcf644dad0fd40
Jose Mauricio <grandemau@gmail.com>
2023-09-28 21:11:49 -0300

    Adicionado criar, atualizar e deletar

--- noticia/urls.py
+++ noticia/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import detalhar, listar
+from .views import detalhar, listar, cadastrar, atualizar, deletar
 
 
 urlpatterns = [
     path('', listar, name='listar'),
-    path('<int:id>', detalhar, name='detalhar')
+    path('<int:id>', detalhar, name='detalhar'),
+    path('cadastrar/', cadastrar, name='cadastrar'),
+    path('atualizar/<int:id>', atualizar, name='atualizar'),
+    path('deletar/<int:id>', deletar, name='deletar')
 ]
--- noticia/views.py
+++ noticia/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Autor
+from .forms import AutorForm
 # Create your views here.
 
 def home(request):
@@ -12,3 +13,31 @@ def listar(request):
 def detalhar(request, id):
     autor = Autor.objects.get(id=id)
     return render(request, 'detalhar.html', {'autor':autor})
+
+def cadastrar(request):
+    if request.method == "POST":
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cadastrar")
+    else:
+         form = AutorForm()
+         return render(request, 'cadastrar.html', {'form': form})
+    
+def atualizar(request, id):
+    autor = Autor.objects.get(id=id)
+    form = AutorForm(instance=autor)
+    if request.method == "POST":
+        form = AutorForm(request.POST, instance=autor)
+        if form.is_valid():
+            form.save()
+            return redirect("atualizar", id=id)
+        else:
+            return render(request, 'atualizar.html', {'form': form})
+    else:
+         return render(request, 'atualizar.html', {'form': form})
+
+def deletar(request, id):
+    autor = Autor.objects.get(id=id)
+    autor.delete()
+    return redirect('listar')
