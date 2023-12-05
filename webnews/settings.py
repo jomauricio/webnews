e02f0cf114cf66ec588cb510a1bb34e7183a8d3b
valpython <114414268+valpython@users.noreply.github.com>
2023-12-05 19:39:43 -0300

    Alterado BD

--- webnews/settings.py
+++ webnews/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'webnews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "webnews",
+#         "USER": "postgres",
+#         "PASSWORD": "senac",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "webnews",
-        "USER": "postgres",
-        "PASSWORD": "senac",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
     }
 }
 
